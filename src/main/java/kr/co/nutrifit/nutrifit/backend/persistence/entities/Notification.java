871ee8714dd379c6a408d2904c532c45290a23d6
devyourown <hyojoon56@gmail.com>
2024-08-15 21:57:32 +0900

    feat : add entities

---
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/entities/Notification.java
@@ -0,0 +1,28 @@
+package kr.co.nutrifit.nutrifit.backend.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String message;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+}
