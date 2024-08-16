c6bb444ea5df3622a899a41765dd6ee7b4c8823b
devyourown <hyojoon56@gmail.com>
2024-08-16 18:57:11 +0900

    feat : add getOrderShip

--- src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/ShippingRepository.java
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/ShippingRepository.java
@@ -3,5 +3,8 @@ package kr.co.nutrifit.nutrifit.backend.persistence;
 import kr.co.nutrifit.nutrifit.backend.persistence.entities.Shipping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShippingRepository extends JpaRepository<Shipping, Long> {
+    Optional<Shipping> findByOrderId(Long orderId);
 }
