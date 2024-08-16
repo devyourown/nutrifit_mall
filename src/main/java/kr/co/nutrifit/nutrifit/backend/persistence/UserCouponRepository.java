3fda7367321c7108c2a933cd1133e40696c9b2cb
devyourown <hyojoon56@gmail.com>
2024-08-16 13:56:07 +0900

    fix : coupon use check

--- src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/UserCouponRepository.java
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/UserCouponRepository.java
@@ -1,5 +1,7 @@
 package kr.co.nutrifit.nutrifit.backend.persistence;
 
+import kr.co.nutrifit.nutrifit.backend.persistence.entities.Coupon;
+import kr.co.nutrifit.nutrifit.backend.persistence.entities.User;
 import kr.co.nutrifit.nutrifit.backend.persistence.entities.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +9,5 @@ import java.util.Optional;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
     Optional<UserCoupon> findByIdAndUserId(Long id, Long userId);
+    boolean existsByUserAndCoupon(User user, Coupon coupon);
 }
