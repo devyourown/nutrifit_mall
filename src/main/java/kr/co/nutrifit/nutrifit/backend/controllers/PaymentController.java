7ed708775f83cc65b89408450265bccce0e5e8d0
devyourown <hyojoon56@gmail.com>
2024-08-19 13:35:26 +0900

    fix : change return PaymentDTO

--- src/main/java/kr/co/nutrifit/nutrifit/backend/controllers/PaymentController.java
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/controllers/PaymentController.java
@@ -2,7 +2,6 @@ package kr.co.nutrifit.nutrifit.backend.controllers;
 
 import jakarta.validation.Valid;
 import kr.co.nutrifit.nutrifit.backend.dto.PaymentDto;
-import kr.co.nutrifit.nutrifit.backend.persistence.entities.Payment;
 import kr.co.nutrifit.nutrifit.backend.security.UserAdapter;
 import kr.co.nutrifit.nutrifit.backend.services.PaymentService;
 import lombok.RequiredArgsConstructor;
@@ -19,26 +18,26 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/create")
-    public ResponseEntity<Payment> createPayment(
+    public ResponseEntity<PaymentDto> createPayment(
             @AuthenticationPrincipal UserAdapter userAdapter,
             @RequestBody @Valid PaymentDto paymentDto
             ) {
-        Payment payment = paymentService.createPayment(userAdapter.getUser(), paymentDto);
-        return ResponseEntity.ok(payment);
+        PaymentDto result = paymentService.createPayment(userAdapter.getUser(), paymentDto);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Payment> getPayment(
+    public ResponseEntity<PaymentDto> getPayment(
             @PathVariable Long id,
             @AuthenticationPrincipal UserAdapter userAdapter
     ) {
-        Payment payment = paymentService.getPaymentByIdAndUser(id, userAdapter.getUser());
-        return ResponseEntity.ok(payment);
+        PaymentDto paymentDto = paymentService.getPaymentByIdAndUser(id, userAdapter.getUser());
+        return ResponseEntity.ok(paymentDto);
     }
 
     @GetMapping("/user")
-    public ResponseEntity<List<Payment>> getUserPayments(@AuthenticationPrincipal UserAdapter userAdapter) {
-        List<Payment> payments = paymentService.getPaymentsByUser(userAdapter.getUser());
+    public ResponseEntity<List<PaymentDto>> getUserPayments(@AuthenticationPrincipal UserAdapter userAdapter) {
+        List<PaymentDto> payments = paymentService.getPaymentsByUser(userAdapter.getUser());
         return ResponseEntity.ok(payments);
     }
 }
