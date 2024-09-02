ac6729578a39a4e6627d561363ece9091821b187
devyourown <hyojoon56@gmail.com>
2024-09-02 11:52:08 +0900

    feat : updatecart add

--- src/main/java/kr/co/nutrifit/nutrifit/backend/controllers/CartController.java
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/controllers/CartController.java
@@ -6,6 +6,7 @@ import kr.co.nutrifit.nutrifit.backend.security.UserAdapter;
 import kr.co.nutrifit.nutrifit.backend.services.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -35,6 +36,19 @@ public class CartController {
         return ResponseEntity.status(201).header(HttpHeaders.CONTENT_TYPE,
                 MediaType.TEXT_PLAIN_VALUE + ";charset=" + StandardCharsets.UTF_8)
                 .body("상품이 장바구니에 추가되었습니다.");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> changeCart(@AuthenticationPrincipal UserAdapter user,
+                                             @RequestBody List<CartItemDto> items) {
+        try {
+            cartService.updateCart(user.getUser(), items);
+            return ResponseEntity.ok("장바구니가 성공적으로 변경되었습니다.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
+        }
     }
 
     @GetMapping("/items")
