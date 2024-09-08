84684f5372a42b02cda0bd3c5057ccaf99b0ffd6
devyourown <hyojoon56@gmail.com>
2024-09-08 16:41:46 +0900

    chore : OAUTH redirect uri change

--- src/main/java/kr/co/nutrifit/nutrifit/backend/controllers/OAuthController.java
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/controllers/OAuthController.java
@@ -20,7 +20,6 @@ public class OAuthController {
             UserDto userDto = oAuthService.changeUsername(request.getEmail(), request.getUsername());
             return ResponseEntity.ok(userDto);
         } catch (Exception e) {
-            System.out.println(e);
             return ResponseEntity.badRequest().body("다시 시도해 주세요.");
         }
     }
