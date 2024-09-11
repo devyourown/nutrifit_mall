7be35570122d8e33db357765eec94cdfbc713db8
devyourown <hyojoon56@gmail.com>
2024-09-11 17:22:14 +0900

    feat : try to remove N + 1 problem with user

--- src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/UserRepository.java
+++ src/main/java/kr/co/nutrifit/nutrifit/backend/persistence/UserRepository.java
@@ -1,13 +1,16 @@
 package kr.co.nutrifit.nutrifit.backend.persistence;
 
 import kr.co.nutrifit.nutrifit.backend.persistence.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {})
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    @EntityGraph(attributePaths = {})
     Optional<User> findByEmail(String email);
 }
